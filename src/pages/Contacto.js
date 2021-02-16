18906d6bb09b5c606d0e2f5a42b868f26b69d5f7
Genesis1212 <genesismudanzas@gmail.com>
2021-02-16 21:44:03 +0100

    updates en la noche

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import './Contacto.css';
+import Footer from "../components/Footer";
 import Map from '../components/Map';
 import credentials from '../Apis/credentials';
 const mapUrl = `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${credentials.mapsKey}`;
 
 function Contacto() {
     return (
-        <div>
-            <div className="contacto">
+        <div className="contacto">
+            <div className="imageContacto">
         
             </div>
+            <div className="contact__map">
+                <Map 
+                
+                    googleMapURL= {mapUrl}
+                    containerElement= {<div style={{height: '400px', width: "400px", flex:"1", marginBottom: "20px"}}></div>}
+                    mapElement= {<div style={{height: '100%'}}></div>}
+                    loadingElement= {<p>Cargando</p>}
 
-            <Map 
-                googleMapURL= {mapUrl}
-                containerElement= {<div style={{height: '400px'}}></div>}
-                mapElement= {<div style={{height: '100%'}}></div>}
-                loadingElement= {<p>Cargando</p>}
-
-            />
-         
-
+                />
+            </div>
+            <Footer />
         </div>
     )
 }
